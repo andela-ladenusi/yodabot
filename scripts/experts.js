366dff6a533445cf12b99bbc0bf999f3913ce41b
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-06-21 17:52:33 +0100

    changed the format of an answer

--- scripts/experts.js
+++ scripts/experts.js
@@ -3,19 +3,21 @@ var apiHost 	= process.env.YODABOT_API_URL || 'http://localhost:5555';
 
 module.exports = function (robot) {
 	robot.router.post('/experts', function (req, res) {
-    var i, question, expert, expertsObj = req.body;
+    var i, qid, question, expert, expertsObj = req.body;
     console.log(expertsObj);
 
     for(i = 0; i < expertsObj.experts.length; i++) {
     	expert 		= expertsObj.experts[i];
     	question 	= expertsObj.question;
+    	qid				= question.id.substr(-8).toLowerCase();
+
     	if(expert.username) {
     		var attachment 		= {
 					content					: {
 						color 				: '#439FE0',
 						fallback			: "A question requires your expertise",
 						pretext				: '*A Question Requires Your Expertise*',
-						author_name		: 'qid: ' + question.id.substr(-8).toLowerCase(),
+						author_name		: 'qid: ' + qid,
 						mrkdwn_in			: ['text', 'pretext']
 					},
 					channel					: expert.username
@@ -24,9 +26,9 @@ module.exports = function (robot) {
 				attachment.content.text = '*Question:* ' + question.body
 																// + '\n>_If you wish to decline this question, click_'
 																// + ' <http://localhost:8080/question/decline|Decline>'
-																+ '\n\n_Give your answer in this format:_ `a[xxxxxxxx] Your answer`'
-																+ '\n_Where_ `xxxxxxxx` _is the_ *qid* _of the question above_';
-	    	robot.emit('slack-attachment', attachment);
+																+ '\n\n_Give your answer in this format:_ `a[' + qid + '] Your answer`';
+																
+	    	return robot.emit('slack-attachment', attachment);
 	    }
 	    console.log('Question has been sent to - ', expert.username);
     }
