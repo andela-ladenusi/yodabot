cf304f97c9ebed831500f82969fc1978e20b076f
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-06-21 17:13:54 +0100

    separated concerns and refactored data processes

---
+++ scripts/answers.js
@@ -0,0 +1,44 @@
+var _ 					= require('lodash');
+var apiHost 		= process.env.YODABOT_API_URL || 'http://localhost:5555';
+
+module.exports 	= function (robot) {
+
+	robot.hear(/a\[(.*)\] (.*)/i, function (response) {
+    var answer = {
+      slack					: response.message.user.id,
+      qid 					: response.match[1],
+      content		 		: response.match[2]
+    };
+
+    response.send('Thank you for your response!');
+
+    robot.http(apiHost + '/answers')
+    .headers({'Content-Type': 'application/json'})
+    .post(JSON.stringify(answer))(function (err, res, body) {
+      if (err) {
+        console.log('Encountered an error - ' + err);
+        return err;
+      }
+      if (body) {
+      	var data = JSON.parse(body);
+      	var attachment 		= {
+					content					: {
+						color 				: 'good',
+						fallback			: "Your question has received a response!",
+						pretext				: '*Your Question Has Received A Response*',
+						author_name		: 'Question: ' + data.body,
+						mrkdwn_in			: ['text', 'pretext']
+					},
+					channel					: data.username
+				};
+
+				attachment.content.text = '*Response:* ' + data.answer.content;
+																// + '\n>_Was this answer helpful?_'
+																// + ' <http://localhost:8080/answers/yes?uname=' + data.username + '|Yes>'
+																// + ' <http://localhost:8080/answers/no|No>';
+
+	    	return robot.emit('slack-attachment', attachment);
+      }
+    });
+  });
+};
