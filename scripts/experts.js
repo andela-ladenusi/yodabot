4baed335868542437b35ba3c24543e67b5f6427b
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-06-25 16:13:49 +0100

    added new check for empty experts

--- scripts/experts.js
+++ scripts/experts.js
@@ -4,11 +4,34 @@ var apiHost 	= process.env.YODABOT_API_URL || 'http://localhost:5555';
 module.exports = function (robot) {
 	robot.router.post('/experts', function (req, res) {
     var i, qid, question, expert, expertsObj = req.body;
+    question 	= expertsObj.question;
     console.log(expertsObj);
+    if (!expertsObj.experts) {
+    	var attachment 		= {
+				content					: {
+					color 				: 'warning',
+					fallback			: ":cry: There are currently no experts for your question",
+					pretext				: ':cry: *There are currently no experts for your question*',
+					text					: '_Why not try to ask the same question with different & simpler tags, maybe?_',
+					mrkdwn_in			: ['fallback', 'text', 'pretext']
+				},
+				channel					: question.username
+			};
+
+			robot.emit('slack-attachment', attachment);
+			setTimeout(function () {
+    		attachment.channel = 'yoda-log';
+    		attachment.content.color = 'danger';
+    		attachment.content.pretext = '*New question from* `' + question.username + '`';
+    		attachment.content.fallback = attachment.content.pretext;
+    		attachment.content.text = '*Question:* ' + question.body;
+    		robot.emit('slack-attachment', attachment);
+    	}, 2000);
+    	return res.status(404).send('I am sorry, but there are currently no experts for your question');
+    }
 
     for (i = 0; i < expertsObj.experts.length; i++) {
     	expert 		= expertsObj.experts[i];
-    	question 	= expertsObj.question;
     	qid				= question.id.substr(-8).toLowerCase();
 
     	if (expert.username && (expert.slack !== question.userId)) {
