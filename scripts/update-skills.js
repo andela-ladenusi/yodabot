713d956e1e826d1d53b7db9d4e7e1469ce310f80
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-06-25 11:54:42 +0100

    created room for removing skills and updated the help file

--- scripts/update-skills.js
+++ scripts/update-skills.js
@@ -2,15 +2,35 @@ var _         = require('lodash');
 var apiHost   = process.env.YODABOT_API_URL || 'http://localhost:5555';
 
 module.exports = function (robot) {
-  robot.hear(/skillset: #\[(.*)\]/i, function (response) {
+  robot.hear(/add-skill #\[(.*)\]/i, function (response) {
+    console.log(response.match[1]);
+    var slackID   = response.message.user.id;
+    var skills    = {
+      skills      : response.match[1].replace(/ /g, '').split(',')
+    };
 
-    var skills = JSON.stringify({
-      skills: response.match[0]
-    });
-
-    robot.http(apiHost + '/users/skills')
+    robot.http(apiHost + '/users/' + slackID + '/skills/add')
     .headers({'Content-Type': 'application/json'})
-    .post(skills)(function (err, res, body) {
+    .post(JSON.stringify(skills))(function (err, res, body) {
+      if(err) {
+        console.log('Encountered an error - ' + err);
+        return 'Encountered an error - ' + err;
+      }
+      console.log(body);
+      return 'Skills successfully updated';
+    });
+    response.send('Thank you for updating your skills.');
+  });
+
+  robot.hear(/remove-skill #\[(.*)\]/i, function (response) {
+    var slackID   = response.message.user.id;
+    var skills    = {
+      skills      : response.match[1].replace(/ /g, '').split(',')
+    };
+
+    robot.http(apiHost + '/users/' + slackID + '/skills/remove')
+    .headers({'Content-Type': 'application/json'})
+    .post(JSON.stringify(skills))(function (err, res, body) {
       if(err) {
         console.log('Encountered an error - ' + err);
         return 'Encountered an error - ' + err;
